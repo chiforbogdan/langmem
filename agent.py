6a1147091284d7ce214ee45fed4b17123558868c
Bogdan-Cosmin Chifor <chiforbogdan@yahoo.com>
2026-05-04 17:25:58 +0400

    Remove print
    Signed-off-by: Bogdan-Cosmin Chifor <bogdan.chifor@tii.ae>

--- agent.py
+++ agent.py
@@ -6,7 +6,6 @@ from tools.memory_tools import memory_tools
 
 async def run_agent(messages: list) -> str:
     llm = ChatGroq(model="qwen/qwen3-32b", temperature=0)
-    print(f"promp {load_system_prompt()}")
     agent = create_react_agent(llm, tools=memory_tools, prompt=load_system_prompt())
     result = await agent.ainvoke({"messages": messages})
     return result["messages"][-1].content
